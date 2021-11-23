84e391d575b2047be7cc74d1ad782764ba9c1172
Joli Holmes <holmesjoli@gmail.com>
2021-11-23 17:11:08 -0500

    added semicolons

--- js/carousel.js
+++ js/carousel.js
@@ -6,12 +6,12 @@ showSlides(slideIndex);
 // Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
-}
+};
 
 // Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
-}
+};
 
 function showSlides(n) {
     var i;
